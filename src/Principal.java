57aa6c4d7479d1e6631a1e8d3c19f2e2af883ae5
Rosa Mateos <rmmateov@gmail.com>
2019-05-10 10:50:00 +0200

    sexto commit

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Línea 1 modificada");
+		System.out.println("Línea 1 añadiendo de nuevo");
 		//System.out.println("Línea 2");
 		System.out.println("Línea 3");
 		System.out.println("Línea 4");
